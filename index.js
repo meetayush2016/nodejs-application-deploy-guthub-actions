9a21890d1bf9a1903eaa22ae9b7ed6b39377a152
Ayush <meetayush2016@gmail.com>
2026-03-19 15:07:28 +0530

    added html file for portfolio

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express();
 const PORT = process.env.PORT ?? 8080;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.get('/', (req, res) => {
-    return res.send('Hello World! this is V2-second-third TATA TATA TATA TATA' );
-}); 
+    const html = readFileSync(join(__dirname, 'portfolio.html'), 'utf-8');
+    res.setHeader('Content-Type', 'text/html');
+    return res.send(html);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
